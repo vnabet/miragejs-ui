68e62e0ef1a4fb2a107308ab8c9c257d6c11a7b1
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2022-12-01 17:28:25 +0100

    add poppins font

--- src/MirageUI.svelte
+++ src/MirageUI.svelte
@@ -18,46 +18,51 @@
 <div class="mirage">
   <div class:opened={$opened}>
     <Toggle checked={$started} on:toggle="{startstop}"></Toggle>
+    Vincent
   </div>
   <MainButton started={$started}></MainButton>
 </div>
 
 <style>
-:global(body) {
-  min-height: 100vh;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
-.mirage {
-  box-sizing: border-box;
-  display: block;
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  margin: 0 60px 30px 0;
-  z-index: 99;
-  color: black;
-}
+  :global(body) {
+    min-height: 100vh;
+  }
 
-.mirage * {
-  box-sizing: border-box;
-}
+  .mirage {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+    box-sizing: border-box;
+    display: block;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin: 0 60px 30px 0;
+    z-index: 99;
+    color: black;
+  }
 
-.mirage>div {
-  width: 200px;
-  height: 200px;
-  padding: 5px;
-  background-color: white;
-  border: 1px solid rgba(0,0,0,.1);
-  border-radius: 15px;
-  box-shadow: 15px 15px 40px rgba(0,0,0,.2);
-  transition: all;
-  transition-duration: .5s;
-  transform: translateX(1000px) ;
-  opacity: 0;
-}
+  .mirage * {
+    box-sizing: border-box;
+  }
 
-.mirage>div.opened {
-  transform: translateX(0);
-  opacity: 1;
-}
+  .mirage>div {
+    width: 200px;
+    height: 200px;
+    padding: 5px;
+    background-color: white;
+    border: 1px solid rgba(0,0,0,.1);
+    border-radius: 15px;
+    box-shadow: 15px 15px 40px rgba(0,0,0,.2);
+    transition: all;
+    transition-duration: .5s;
+    transform: translateX(1000px) ;
+    opacity: 0;
+  }
+
+  .mirage>div.opened {
+    transform: translateX(0);
+    opacity: 1;
+  }
 </style>
