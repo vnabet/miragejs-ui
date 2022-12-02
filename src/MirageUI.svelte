fc82b64a2751ac71e5a18dcf538a354e1beab598
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2022-12-02 22:46:13 +0100

    popup component

--- src/MirageUI.svelte
+++ src/MirageUI.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 
   import MainButton from './components/MainButton.svelte';
-  import Toggle from './components/Toggle.svelte';
+  import Popup from './components/Popup.svelte';
 
   import {opened, started} from './stores/MirageUI.store';
-
-  function startstop(ev:CustomEvent) {
-    started.set(ev.detail);
-  }
-
 
   document.addEventListener('keydown', (event:KeyboardEvent) => {
     if(event.key === 'Escape' && $opened === true) opened.set(false);
@@ -16,12 +11,7 @@
 </script>
 
 <div class="mirage">
-  <div class:opened={$opened}>
-    <div style="display: flex;align-items:center; gap:10px">
-      <label>Started</label>
-      <Toggle checked={$started} on:toggle="{startstop}"></Toggle>
-    </div>
-  </div>
+  <Popup></Popup>
   <MainButton started={$started}></MainButton>
 </div>
 
@@ -47,24 +37,5 @@
 
   .mirage * {
     box-sizing: border-box;
-  }
-
-  .mirage>div {
-    width: 200px;
-    height: 200px;
-    padding: 5px;
-    background-color: white;
-    border: 1px solid rgba(0,0,0,.1);
-    border-radius: 15px;
-    box-shadow: 15px 15px 40px rgba(0,0,0,.2);
-    transition: all;
-    transition-duration: .5s;
-    transform: translateX(1000px) ;
-    opacity: 0;
-  }
-
-  .mirage>div.opened {
-    transform: translateX(0);
-    opacity: 1;
   }
 </style>
---
+++ src/components/Popup.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import Toggle from './Toggle.svelte';
+
+  import {opened, started} from '../stores/MirageUI.store';
+
+  function startstop(ev:CustomEvent) {
+    started.set(ev.detail);
+  }
+</script>
+
+<div class:opened={$opened} class="popup">
+  <div class="header">MirageUI</div>
+  <div class="content">
+    <div style="display: flex;align-items:center; gap:10px">
+      <label>Started</label>
+      <Toggle checked={$started} on:toggle="{startstop}"></Toggle>
+    </div>
+  </div>
+</div>
+
+<style>
+  .popup {
+    width: 200px;
+    height: 200px;
+    /* padding: 5px; */
+    background-color: white;
+    border: 1px solid rgba(0,0,0,.1);
+    border-radius: 15px;
+    box-shadow: 15px 15px 40px rgba(0,0,0,.2);
+    transition: all;
+    transition-duration: .5s;
+    transform: translateX(1000px) ;
+    opacity: 0;
+  }
+
+  .popup.opened {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  .header {
+    height: 2rem;
+    background: linear-gradient(328deg, rgba(2,0,36,1) 0%, rgba(5,199,126,1) 0%, rgba(21,125,85,1) 100%);
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px 15px 0 0;
+    color: white;
+  }
+
+  .content {
+    padding: 10px;
+  }
+</style>
