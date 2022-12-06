26701979b717484ffc4d088efa6d846ed9f2f991
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2022-12-06 17:28:31 +0100

    svelte-jsoneditor test

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
   import type { IPet } from "./models/IPet";
 
+  import { JSONEditor } from 'svelte-jsoneditor'
+
 
 
   let pets:IPet[] = [];
+
+  let content = {
+    text: undefined, // can be used to pass a stringified JSON document instead
+    json: {
+      array: [1, 2, 3],
+      boolean: true,
+      color: '#82b92c',
+      null: null,
+      number: 123,
+      object: { a: 'b', c: 'd' },
+      string: 'Hello World'
+    }
+  }
 
 async function getPets() {
   pets = await fetch('https://petstore.swagger.io/v2/pet/findByStatus?status=available').then((res) => res.json())
@@ -12,6 +27,10 @@ async function getPets() {
 </script>
 
 <main>
+  <div class="my-json-editor jse-theme-dark">
+    <JSONEditor bind:content mainMenuBar={true} navigationBar={true} indentation={' '} width="1200"></JSONEditor>
+  </div>
+  
   <button on:click={getPets}>Load Pets</button>
   <ul>
     {#each pets as pet}
@@ -21,5 +40,15 @@ async function getPets() {
 </main>
 
 <style>
+
+@import 'svelte-jsoneditor/themes/jse-theme-dark.css';
+
+.my-json-editor {
+  /* --jse-font-family: 'Poppins', sans-serif; */
+  /* over all fonts, sizes, and colors */
+  /* over all fonts, sizes, and colors */
+  --jse-theme-color: rgb(5,199,126);
+  --jse-theme-color-highlight: rgb(21,125,85);
+}
   
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  optimizeDeps: {
+    include: [
+      'ajv-dist',
+      'immutable-json-patch',
+      'lodash-es',
+      '@fortawesome/free-regular-svg-icons',
+      'jmespath'
+    ]
+  }
 })
